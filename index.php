34494395e67194f870668ce3be9ea52aea9e15d6
Vojtěch Divoký <divokyvojta@gmail.com>
2021-06-14 16:09:47 +0200

    řešení overlappování

--- index.php
+++ index.php
@@ -29,34 +29,26 @@
 
     html {
         scroll-behavior: smooth;
-        scroll-padding-top: 150px;
     }
 
-    #slide-5 {
+    .overlap {
         position: relative;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        z-index: 2;
     }
 
-    #slide-5 .bcg {
-        background-image: url('../img/bcg_slide-5.jpg')
+    .overlap h1{
+        background-color:red;
     }
 
-    #slide-5 .bcg2 {
-        background-image: url('../img/bcg_slide-5b.jpg');
-        position: fixed;
-        bottom: 0;
+    .overlapBack{
+        position: absolute;
+        top: 0;
         left: 0;
-        opacity: 0;
-        z-index: 1
-    }
-
-    #slide-5 .bcg3 {
-        background: none;
-        background-color: #010101;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        opacity: 0;
-        z-index: 2
+        background-color: black;
+        z-index: 1;
     }
     </style>
 </head>
@@ -64,49 +56,22 @@
 <body>
 
     <div class="container-fluid bgImage" style="background-image: url(imgs/0.jpg);"></div>
-
-    <section id="slide-5" style="background-image: url(imgs/1.jpg);">
-        <div class="bcg">&nbsp;</div>
-        <div class="bcg bcg2" data-bottom-top="opacity: 0;" data--33p-top="opacity: 0;" data--66p-top="opacity: 1;"
-            data-anchor-target="#slide-5">
-            <div class="hsContainer">
-                <div class="hsContent" data-bottom-top="opacity: 0;" data-center="opacity: 1"
-                    data-anchor-target="#slide-5">
-                    <h2>Fixed element fading in and out</h2>
-                </div>
-            </div>
+    <div class="container-fluid con" style="background-image: url(imgs/1.jpg);">
+        <div class="overlap overlapBack" data-center-top="opacity: 0;" data-top="opacity: .7;"></div>
+        <div class="p-5 text-center text-light overlap" data-0="opacity: 0;" data-500="opacity: 1;">
+            <h1>Hello, world!</h1>
+            <hr class="bg-white border-5 border-top border-white">
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet pharetra
+                semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam neque. Duis viverra diam
+                non justo. Nulla non arcu lacinia neque faucibus fringilla. Maecenas fermentum, sem in pharetra
+                pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Aenean vel massa quis
+                mauris vehicula lacinia. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis
+                condimentum augue id magna semper rutrum. Morbi imperdiet, mauris ac auctor dictum, nisl ligula
+                egestas nulla, et sollicitudin sem purus in lacus.
+            </p>
         </div>
-        <div class="bcg bcg3" data-300-bottom="opacity: 0;" data-100-bottom="opacity: 1;" data-anchor-target="#slide-5">
-            <div class="hsContainer">
-                <div class="hsContent" data-100-bottom="opacity: 0;" data-bottom="opacity: 1;"
-                    data-anchor-target="#slide-5">
-                    <h2>The End</h2>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!--<div class="container-fluid px-3 py-5 con" style="background-image: url(imgs/1.jpg);" data-0="filter: blur(0);"
-        data-center-top="filter: blur(10px);">
-        <div class="row p-5  text-center">
-            <div class="col text-light">
-                <h1>Hello, world!</h1>
-                <hr class="bg-white border-5 border-top border-white">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet pharetra
-                    semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam neque. Duis viverra diam
-                    non justo. Nulla non arcu lacinia neque faucibus fringilla. Maecenas fermentum, sem in pharetra
-                    pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Aenean vel massa quis
-                    mauris vehicula lacinia. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis
-                    condimentum augue id magna semper rutrum. Morbi imperdiet, mauris ac auctor dictum, nisl ligula
-                    egestas nulla, et sollicitudin sem purus in lacus.
-                </p>
-            </div>
-        </div>
-    </div>-->
-
-
-    <div class="container-fluid bgImage" style="background-image: url(imgs/05.jpg);"></div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
