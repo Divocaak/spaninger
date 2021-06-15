63048b7a446a1e319c8da0bec82b442f69e60286
Vojtěch Divoký <divokyvojta@gmail.com>
2021-06-15 13:35:19 +0200

    ovelay vyřešen, přidán navbar

--- index.php
+++ index.php
@@ -28,41 +28,81 @@
         background-attachment: fixed;
     }
 
+    .con {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr;
+    }
+
+    #topShadow {
+        -webkit-box-shadow: 0px 0px 15px 0px #000000;
+        box-shadow: 0px 0px 15px 0px #000000;
+    }
+
     .filter {
+        grid-column-start: 1;
+        grid-column-end: 4;
+        grid-row-start: 1;
+        grid-row-end: 4;
         background-color: black;
-        z-index:1;
+        z-index: 1;
+        margin: 0px -12px 0px -12px;
     }
 
     .content {
-        z-index:2;
+        grid-column-start: 2;
+        grid-column-end: 3;
+        grid-row-start: 2;
+        grid-row-end: 3;
+        z-index: 2;
     }
     </style>
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" data-0="opacity: 0;" data-250="opacity: .9;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">txt</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="#topShadow">btn1</a>
+                    <a class="nav-link" href="#href2">btn2</a>
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <div class="container-fluid bgImage" style="background-image: url(imgs/0.jpg);"></div>
-    <div class="container-fluid con" style="background-image: url(imgs/1.jpg);">
-        <div class="filter h-100 p-5 row align-items-center justify-content-center" data-center-top="opacity: 0;" data-top="opacity: .7;">
-            <div class="col text-center content" data-0="opacity: 0" data-top="opacity: 1">
-                <h1>Hello, world!</h1>
-                <hr class="bg-white border-5 border-top border-white">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet
-                    pharetra
-                    semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam neque. Duis
-                    viverra
-                    diam
-                    non justo. Nulla non arcu lacinia neque faucibus fringilla. Maecenas fermentum, sem in
-                    pharetra
-                    pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Aenean vel massa
-                    quis
-                    mauris vehicula lacinia. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Duis
-                    condimentum augue id magna semper rutrum. Morbi imperdiet, mauris ac auctor dictum, nisl
-                    ligula
-                    egestas nulla, et sollicitudin sem purus in lacus.
-                </p>
-            </div>
+    <div class="container-fluid con" id="topShadow" style="background-image: url(imgs/1.jpg);">
+        <div class="filter" data-center-top="opacity: 0;" data-top="opacity: .7;" data-center-bottom="opacity: 0;">
+        </div>
+        <div class="text-center content text-light" data--100-bottom="opacity: 0;" data-center="opacity: 1;"
+            data-100-top="opacity: 0;">
+            <h1>text 1</h1>
+            <hr class="bg-white border-5 border-top border-white">
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet
+                pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam neque. Duis
+                viverra diam non justo.
+            </p>
+        </div>
+    </div>
+    <div class="container-fluid con" id="href2" style="background-image: url(imgs/1.jpg);">
+        <div class="filter" data-center-top="opacity: 0;" data-top="opacity: .7;" data-center-bottom="opacity: 0;">
+        </div>
+        <div class="text-center content text-light" data--100-bottom="opacity: 0;" data-center="opacity: 1;"
+            data-100-top="opacity: 0;">
+            <h1>text 2</h1>
+            <hr class="bg-white border-5 border-top border-white">
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet
+                pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam neque. Duis
+                viverra diam non justo.
+            </p>
         </div>
     </div>
 
