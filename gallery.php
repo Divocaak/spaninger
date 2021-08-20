c29edc2bed59651fb0e3f74e037c7cba62bdb2c0
Vojtěch Divoký <divokyvojta@gmail.com>
2021-08-20 23:42:27 +0200

    rozpracována galerie

--- gallery.php
+++ gallery.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Úvod</title>
+    <title>Galerie</title>
 
     <link rel="stylesheet" href="styles/index.css">
 
@@ -22,31 +22,61 @@
 </head>
 
 <body>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
+    <div class="modal fade" id="galModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Nadpis</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <div id="carouselExampleControls" class="carousel slide w-50" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <?php
+                        echo '<div class="carousel-item active">
+                                <img src="imgs/0.jpg" class="d-block w-100">
+                              </div>';
+                        ?>
+                        <div class="carousel-item">
+                            <img src="imgs/1.jpg" class="d-block w-100">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imgs/2.jpg" class="d-block w-100">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
     <div class="row">
-        <div class="card m-1 p-1" style="width: 18rem;">
-            <img src="imgs/0.jpg" class="card-img-top">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                    <button type="button" class="btn btn-primary stretched-link" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-                </div>
-        </div>
+        <?php
+        $catNames = ["Zahrady", "Kuchyně"];
+        foreach (glob("imgs/gal/*") as $key=>$folder) {
+          echo "<h1>" . $catNames[$key] . "</h1>";
+          foreach (glob($folder . "/*") as $img) {
+            echo '<div class="card m-1 p-1" style="width: 18rem;">
+                    <img src="' . $img . '/0.jpg" class="card-img-top">
+                    <div class="card-body">
+                      <p class="card-text">info</p>
+                      <button type="button" class="btn btn-primary stretched-link" data-bs-toggle="modal"
+                        data-bs-target="#galModal">
+                        Zobrazit více
+                      </button>
+                    </div>
+                  </div>';
+          }
+        }
+        ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
