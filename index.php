d5e99800bc62c16a66a457f96adea48d9752c341
Vojtěch Divoký <divocak@Vojtechs-MacBook-Air.local>
2021-12-25 16:04:40 +0100

    jquery loading content

--- index.php
+++ index.php
@@ -5,19 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <title>Úvod</title>
 
     <link rel="stylesheet" href="styles/index.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
     </script>
 </head>
 
@@ -27,39 +24,120 @@
             <a class="navbar-brand" href="#">
                 <img src="imgs/log.png" alt="" width="90" height="64" class="d-inline-block align-text-top">
 
-            </a><button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                aria-label="Toggle navigation">
+            </a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
-                    <a class="nav-link text-uppercase" href="#menu0">o nás</a>
-                    <a class="nav-link text-uppercase" href="#menu1">Služby</a>
-                    <a class="nav-link text-uppercase" href="#menu2">Reference</a>
-                    <a class="nav-link text-uppercase" href="#menu3">Kontakt</a>
+                    <a class="nav-link text-uppercase menuBtn" id="menu0">o nás</a>
+                    <a class="nav-link text-uppercase menuBtn" id="menu1">Služby</a>
+                    <a class="nav-link text-uppercase menuBtn" id="menu2">Reference</a>
+                    <a class="nav-link text-uppercase menuBtn" id="menu3">Kontakt</a>
                 </div>
             </div>
         </div>
     </nav>
     <div class="container p-0 m-0">
-        <img src="imgs/0.jpg" class="img-fluid w-100 containerContent"/>
+        <img src="imgs/0.jpg" class="img-fluid w-100 containerContent" id="bodyImg" />
         <div class="shape"></div>
         <div class="shape-sm"></div>
         <div class="containerContent containerText text-light">
             <div class="textMask">asdads</div>
-            <h2>O nás</h2>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor.
-                Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Curabitur vitae diam non enim vestibulum interdum.
-                Maecenas libero. Aliquam erat volutpat. Integer lacinia. Nullam rhoncus aliquam metus. Etiam sapien elit, consequat eget,
-                tristique non, venenatis quis, ante. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-                nisi ut aliquid ex ea commodi consequatur? Aliquam in lorem sit amet leo accumsan lacinia. Maecenas sollicitudin.
-                Integer pellentesque quam vel velit. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci.
-                Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
-                aut perferendis doloribus asperiores repellat. Etiam quis quam. Vivamus porttitor turpis ac leo.
-            </p>
+            <div id="content0" class="content">
+                <h2>O nás</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor.
+                    Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Curabitur vitae diam non enim vestibulum interdum.
+                    Maecenas libero. Aliquam erat volutpat. Integer lacinia. Nullam rhoncus aliquam metus. Etiam sapien elit, consequat eget,
+                    tristique non, venenatis quis, ante. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+                    nisi ut aliquid ex ea commodi consequatur? Aliquam in lorem sit amet leo accumsan lacinia. Maecenas sollicitudin.
+                    Integer pellentesque quam vel velit. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci.
+                    Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
+                    aut perferendis doloribus asperiores repellat. Etiam quis quam. Vivamus porttitor turpis ac leo.
+                </p>
+            </div>
+            <div id="content1" class="content">
+                <h2>Služby</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor.
+                    Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Curabitur vitae diam non enim vestibulum interdum.
+                    Maecenas libero. Aliquam erat volutpat. Integer lacinia. Nullam rhoncus aliquam metus. Etiam sapien elit, consequat eget,
+                    tristique non, venenatis quis, ante. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+                    nisi ut aliquid ex ea commodi consequatur? Aliquam in lorem sit amet leo accumsan lacinia. Maecenas sollicitudin.
+                    Integer pellentesque quam vel velit. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci.
+                    Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
+                    aut perferendis doloribus asperiores repellat. Etiam quis quam. Vivamus porttitor turpis ac leo.
+                </p>
+            </div>
+            <div id="content2" class="content">
+                <h2>Reference</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor.
+                    Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Curabitur vitae diam non enim vestibulum interdum.
+                    Maecenas libero. Aliquam erat volutpat. Integer lacinia. Nullam rhoncus aliquam metus. Etiam sapien elit, consequat eget,
+                    tristique non, venenatis quis, ante. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+                    nisi ut aliquid ex ea commodi consequatur? Aliquam in lorem sit amet leo accumsan lacinia. Maecenas sollicitudin.
+                    Integer pellentesque quam vel velit. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci.
+                    Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
+                    aut perferendis doloribus asperiores repellat. Etiam quis quam. Vivamus porttitor turpis ac leo.
+                </p>
+            </div>
+            <div id="content3" class="content">
+                <h2>Kontakt</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris elementum mauris vitae tortor.
+                    Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Curabitur vitae diam non enim vestibulum interdum.
+                    Maecenas libero. Aliquam erat volutpat. Integer lacinia. Nullam rhoncus aliquam metus. Etiam sapien elit, consequat eget,
+                    tristique non, venenatis quis, ante. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+                    nisi ut aliquid ex ea commodi consequatur? Aliquam in lorem sit amet leo accumsan lacinia. Maecenas sollicitudin.
+                    Integer pellentesque quam vel velit. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci.
+                    Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
+                    aut perferendis doloribus asperiores repellat. Etiam quis quam. Vivamus porttitor turpis ac leo.
+                </p>
+            </div>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script>
+        var currContentId;
+        $(document).ready(function() {
+            $("#content0").animate({
+                opacity: 1
+            }, 500);
+            currContentId = "#content0";
+
+            $("#menu0").click(function() {
+                changeBody("#content0", "0");
+            });
+            $("#menu1").click(function() {
+                changeBody("#content1", "1");
+            });
+            $("#menu2").click(function() {
+                changeBody("#content2", "2");
+            });
+            $("#menu3").click(function() {
+                changeBody("#content3", "1");
+            });
+        });
+
+        function changeBody(content, img) {
+            $(currContentId).animate({
+                opacity: 0
+            }, function() {
+                $(this).hide(function(){
+                    $(content).show(function(){
+                        $(this).animate({
+                            opacity: 1
+                        });
+                    });
+                });
+            });
+            currContentId = content;
+
+            var image = $("#bodyImg");
+            image.fadeOut('slow', function() {
+                image.attr('src', 'imgs/' + img + '.jpg');
+                image.fadeIn('fast');
+            });
+        }
+    </script>
 </body>
 
 </html>
